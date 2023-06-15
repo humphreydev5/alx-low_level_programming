772d341849aaa6bfbb3309398fb5bba51a4186dd
humphreydev5 <humphreyikhalea6@gmail.com>
2023-06-15 23:27:24 +0000

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - The entry point for the programming
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
