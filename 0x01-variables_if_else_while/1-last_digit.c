68acbc8080aac698a60ad9cfde0f1e2dfb3d257c
humphreydev5 <humphreyikhalea6@gmail.com>
2023-06-16 18:14:17 +0000

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
+/**
+ * main - Determines if a number is positive, negative or zero.
+ *
+ * Return: Always 0 (Success)
+ */
 
-/* betty style doc for function main goes there */
+
 int main(void)
 {
 	int n;
