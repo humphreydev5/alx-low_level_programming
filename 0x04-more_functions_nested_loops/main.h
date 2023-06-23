b11c13ec91bd5cee51f7717442691ded9595c3ce
humphreydev5 <humphreyikhalea6@gmail.com>
2023-06-23 09:48:33 +0000

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,5 +9,11 @@ int mul(int a, int b);
 int _putchar(char c);
 void print_numbers(void);
 void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
 
 #endif
