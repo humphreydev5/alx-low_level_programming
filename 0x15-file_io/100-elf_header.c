4a3fcf88ee42d819e5b224cd59681a0e84131bc2
humphreydev5 <humphreyikhalea6@gmail.com>
2023-08-07 14:04:44 +0000

    print_class.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -12,7 +12,6 @@
 
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
-void print_class(unsigned char *e_ident);
 void print_data(unsigned char *e_ident);
 void print_version(unsigned char *e_ident);
 void print_abi(unsigned char *e_ident);
