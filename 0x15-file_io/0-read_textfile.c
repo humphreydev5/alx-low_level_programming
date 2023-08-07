5df8fe6d1df80299c89de2d4c9b4ae7ef8392636
humphreydev5 <humphreyikhalea6@gmail.com>
2023-08-07 12:02:23 +0000

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 /**
- * read_textfile- Read text file print to STDOUT.
- * @filename: text file being read
- * @letters: number of letters to be read
+ * read_textfile - To read text file print to STDOUT.
+ * @filename: To text file being read
+ * @letters: number of letters to be read in a file
  * Return: w- actual number of bytes read and printed
  *        0 when function fails or filename is NULL.
  */
